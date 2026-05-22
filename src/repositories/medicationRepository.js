d119b6b8d22950014e5d1fef508f5abc3f27e2c9
tulsi <tulsihirpara28@gmail.com>
2026-05-22 11:06:05 +0530

    feat(mediction): in filter api add user id get data by user id

--- src/repositories/medicationRepository.js
+++ src/repositories/medicationRepository.js
@@ -62,7 +62,7 @@ class MedicationRepository {
   }
 
   async findById(id) {
-    console.log("id", id);
+    // console.log("id", id);
     const result = await db
       .select()
       .from(medication)
@@ -73,16 +73,19 @@ class MedicationRepository {
 
     return result[0] || null;
   }
+  //filtered list
+  async findAllWithFilters({ filter = {}, sort = {}, userId }) {
+    // existing filters
+    const filterWhere = buildMedicationFilters(filter);
 
-  async findAllWithFilters({ filter = {}, sort = {} }) {
-    const where = buildMedicationFilters(filter);
+    // add userId filter
+    const where = filterWhere
+      ? and(filterWhere, eq(medication.userId, userId))
+      : eq(medication.userId, userId);
 
     const orderClause = buildOrderClause(sort);
-
     const rows = await db.select().from(medication).where(where).orderBy(orderClause);
-
     const totalRows = await db.select({ total: count() }).from(medication).where(where);
-
     return {
       rows,
       total: Number(totalRows[0]?.total || 0),
